3566aa0d67a433cc06f571dfc0d9a78c46c932b2
BitchFalcon <46070796+BitchFalcon@users.noreply.github.com>
2019-09-01 10:18:05 +0100

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        forloop(5);
+        recursion(4);
+        tailrecursion(5);
+
+    }
+
+    public static int forloop(int n) {
+
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    public static int recursion(int n) {
+
+        if (n > 1) {
+            return n * recursion(n - 1);
+        }
+
+        return n;
+    }
+
+    public static int tailrecursion(int n) {
+
+        if (n < 1) {
+            return n;
+        }
+
+        return n * tailrecursion(n - 1);
+    }
+
+
+}
+
+
+
